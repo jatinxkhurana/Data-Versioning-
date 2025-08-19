a15bd53c21e806893356bf46ae05f113425acceb
Jatin Khurana <jatinkhuranasirsa@gmail.com>
2025-08-19 22:07:30 +0530

    add cookie cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is to test data versioning using DVC and Git ',
+    author='Jatin Khurana',
+    license='',
+)
